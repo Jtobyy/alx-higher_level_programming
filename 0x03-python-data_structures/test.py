cb67b6702f0171441716bdb34685e3d08783c418
Jtobyy <jtobi8161@gmail.com>
2021-09-12 21:53:49 -0400

    initial commit for 0x03-python-data_structures

---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import 0-print_list_integer
+
+ls = [i for i in range(10)]
+print_list_integer(ls)
---
+++ 0x03-python-data_structures/test/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from ..0-print_list_integer import print_list_integer
+
+ls = [i for i in range(10)]
+print_list_integer(ls)
