398bbd2667100404814deb789ae5b08613dc43b1
Jtobyy <jtobi8161@gmail.com>
2021-09-13 04:22:38 -0400

    1-calculation.py in 0x02-python-import_modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, a + b))
+print("{} - {} = {}".format(a, b, a - b))
+print("{} * {} = {}".format(a, b, a * b))
+print("{} / {} = {}".format(a, b, int(a / b)))
+
+if __name__ == "__main__":
+    pass
