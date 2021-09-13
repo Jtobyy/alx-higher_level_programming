786c594f93bb406014986e7c087de23f6faad8e1
Jtobyy <jtobi8161@gmail.com>
2021-09-13 05:22:04 -0400

    3-infinite_add.py in 0x02-python-import_modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+from functools import reduce
+
+args = sys.argv[1:]
+if len(args) == 0:
+    print("0")
+else:
+    con = map(lambda x: int(x), args)
+    sum_ = reduce(lambda x, y: x + y, con)
+    print("{}".format(sum_))
