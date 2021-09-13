0d42b9e4bdf00875d557328533f010dfc279f4ba
Jtobyy <jtobi8161@gmail.com>
2021-09-12 22:02:51 -0400

    keep up with requruirements

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,3 +4,7 @@ def print_list_integer(my_list=[]):
     for i in my_list:
         print("{}".format(i))
     return
+
+
+if __name__ == "__main__":
+    pass
--- 0x03-python-data_structures/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-import 0-print_list_integer
-
-ls = [i for i in range(10)]
-print_list_integer(ls)
