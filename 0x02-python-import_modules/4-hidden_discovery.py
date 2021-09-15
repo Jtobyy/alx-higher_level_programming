f5b34369a0cccb773f87780d637251574969805f
Jtobyy <jtobi8161@gmail.com>
2021-09-15 23:36:30 +0000

    print only names without a beginning __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,7 @@ import hidden_4
 
 names = dir(hidden_4)
 for mods in names:
-    print("{}".format(mods))
+    if mods[0] != '_' and mods[1] != '_':
+        print("{}".format(mods))
 if __name__ == "__main__":
     pass
