0faf4a1436bb1848165ad2353010cd262fb2bfb0
Jtobyy <jtobi8161@gmail.com>
2021-09-30 04:47:42 +0000

    implement 5-square.py in 0x06

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,34 @@
-#!/usr/bin/pytbon3
-"""defines a square by: (based on 4-square.py)"""
+#!/usr/bin/python3
+"""defines a square by: (based on 0-square.py)"""
 
 
 class Square:
-    """defines a square by: (based on 4-square.py)"""
+    """defines a square by: (based on 0-square.py)"""
+    def __init__(self, size=0):
+        """defines a private attribute of the class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if not isinstance(val, int):
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = val
+
+    def area(self):
+        x = self.size**2
+        return x
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            for i in range(self.size):
+                print('#', end="")
+            print()
