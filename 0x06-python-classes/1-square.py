0866bafaea1d89d706a19c6df2385eb2482e58de
Jtobyy <jtobi8161@gmail.com>
2021-09-30 03:59:15 +0000

    Implement 1-square.py in 0x06

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
-#!/usr/bin/pytbon3
+#!/usr/bin/python3
 """defines a square by: (based on 0-square.py)"""
 
 
 class Square:
     """defines a square by: (based on 0-square.py)"""
     def __init__(self, size):
-        """defines a private attribute of the classes"""
+        """defines a private attribute of the class"""
         self.__size = size
