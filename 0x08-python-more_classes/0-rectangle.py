f45a2cbd30dea3f0cdabdb97f7bab7da04ee8028
root <root@localhost.localdomain>
2021-10-02 21:56:29 -0400

    add tests

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,5 +3,12 @@
 
 
 class Rectangle:
-    """defines a rectangle"""
+    """
+    >>> Rectangle = __import__('0-rectangle').Rectangle
+    >>> my_rectangle = Rectangle()
+    >>> print(type(my_rectangle))
+    <class '0-rectangle.Rectangle'>
+    >>> print(my_rectangle.__dict__)
+    {}
+    """
     pass
