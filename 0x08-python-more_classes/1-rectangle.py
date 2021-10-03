5f77b3659d75746471940ff5369501332f56bbde
Jtobyy <jtobi8161@gmail.com>
2021-10-03 07:16:14 -0400

    strctly use the pythonic way  to tceate attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
     {}
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -40,11 +40,11 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
+
 
 if __name__ == "__main__":
     import doctest
