ae2e2e6c64a7636601f8874f0691a19926e242a4
Jtobyy <jtobi8161@gmail.com>
2021-10-07 11:58:50 -0400

    Unittesting in tests/6-max_integer_test.py

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1, 2, 4, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([4, 2, 3, 1]), 4)
+        self.assertAlmostEqual(max_integer([4, 2, 3, -1]), 4)
+        self.assertAlmostEqual(max_integer([-4, -2, -3, -1]), -1)
+        self.assertAlmostEqual(max_integer([-1]), -1)
