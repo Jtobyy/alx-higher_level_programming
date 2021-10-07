439a6f079915cdcb5424e8ad9bb7c8c55d6a8a09
Jtobyy <jtobi8161@gmail.com>
2021-10-07 11:32:59 -0400

    PEP8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,8 @@ def matrix_divided(matrix, div):
     divides all elements of matrix by div
     '''
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
     new_matrix = []
     mat_len = len(matrix)
     if not isinstance(div, (int, float)):
@@ -15,15 +16,16 @@ def matrix_divided(matrix, div):
     elif div == 0:
         raise ZeroDivisionError("division by zero")
     elif type(matrix[0]) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
     row_len = len(matrix[0])
     for ls in matrix:
         if len(ls) != row_len:
             raise TypeError("Each row of the matrix must have the same size")
         for val in ls:
             if not isinstance(val, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
         sub_matrix = [round(i / div, 2) for i in ls]
         new_matrix.append(sub_matrix)
 
