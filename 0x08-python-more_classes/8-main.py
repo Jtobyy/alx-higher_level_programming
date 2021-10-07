f0169205634e79631525fd2d0eab27990632b504
Jtobyy <jtobi8161@gmail.com>
2021-10-06 22:23:56 -0400

    correct 8

--- 0x08-python-more_classes/8-main.py
+++ 0x08-python-more_classes/8-main.py
@@ -2,7 +2,7 @@
 Rectangle = __import__('8-rectangle').Rectangle
 
 my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 3)
+my_rectangle_2 = Rectangle(4, 8)
 
 if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
     print("my_rectangle_1 is bigger or equal to my_rectangle_2")
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -93,7 +93,7 @@ class Rectangle:
         new_instance.width = size
         new_instance.height = size
         return new_instance
-        
+
     def __del__(self):
         type(self).number_of_instances -= 1
         print ("Bye rectangle...")
