baffdfa76156541648fe90551e266eeacc9c29f8
Jtobyy <jtobi8161@gmail.com>
2021-10-08 06:34:16 -0400

    Implement 1-write_file.py in 0x0B

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,8 +9,6 @@ def write_file(filename="", text=""):
     writes a string to a text file(UTF8) and return the number
     of characters written
     '''
-    with open(text, 'w') as t:
-        with open(filename, 'r') as f:
-            n = t.write(f.read())
+    with open(filename, 'w+') as t:
+        n = t.write(text)
     return n
-
