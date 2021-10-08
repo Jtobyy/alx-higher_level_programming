4e7276c76a80067f058e2f9be9d1295b4d4f6ca9
Jtobyy <jtobi8161@gmail.com>
2021-10-08 02:42:18 -0400

    implement 3-is_kind_of_class.py in 0x0A

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,14 @@ class
 
 
 def is_same_class(obj, a_class):
+    '''
+    Checks if an object is an instance of a specified
+    class
+    '''
     if isinstance(obj, a_class):
+        if a_class == object:
+            return False
         return True
     else:
         return False
+
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Checks if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    Returns True if the object is an instance of, or if the object is an instance 
+    of a class that inherited from, the specified class; otherwise False
+    '''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
+
+
