8294b9f798041fc148d1a769c30e75ff6d096b9d
Jtobyy <jtobi8161@gmail.com>
2021-10-08 03:31:04 -0400

    implement 6-base_geometry.py in 0x0A

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+extended version of 5-base_geometry
+'''
+
+
+class BaseGeometry:
+    '''
+    base geometry class
+    '''
+    def area(self):
+        '''
+        public instance method
+        '''
+        raise Exception("area() is not implemented")
