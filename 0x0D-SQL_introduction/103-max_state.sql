04109e8cb546000b2800a82208ce2ae6fc772739
Jtobyy <jtobi8161@gmail.com>
2021-11-18 12:22:44 +0000

    Task 103, use an alias

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Displays the max temperature of each state (ordered by State name)
-SELECT state, max(value) FROM temperatures GROUP BY state ORDER BY state
+SELECT state, max(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state
