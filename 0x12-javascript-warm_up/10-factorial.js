9bc56da927ed962970f95abb92b4e77f6955db9a
Jtobyy <jtobi8161@gmail.com>
2021-12-16 09:00:05 -0500

    Computest and print a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = parseInt(process.argv[2]);
+function factorial (n) {
+  if (Number.isNaN(arg)) return 1;
+  if (n === 1) return 1;
+  else return n *= factorial(n - 1);
+}
+
+console.log(factorial(arg));
