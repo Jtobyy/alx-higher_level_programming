fb14c1f3cea877f73d9d5aaa94d430da88759225
Jtobyy <jtobi8161@gmail.com>
2021-12-15 19:10:30 -0500

    Adhere to semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
 const arg = parseInt(process.argv[2]);
-if (Number.isNaN(arg))
-    console.log('Not a Number');
-else
-    console.log('My number: ' + arg);    
+if (Number.isNaN(arg)) console.log('Not a number');
+else console.log('My number: ' + arg);
