cd23c525179ea916b24250fccfac35e7e2a9fc87
Jtobyy <jtobi8161@gmail.com>
2021-12-16 09:56:50 -0500

    Clean ups

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,8 @@ const arg = parseInt(process.argv[2]);
 function factorial (n) {
   if (Number.isNaN(arg)) return 1;
   if (n === 1) return 1;
-  else return n *= factorial(n - 1);
+  else n *= factorial(n - 1);
+  return n;
 }
 
 console.log(factorial(arg));
