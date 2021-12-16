a9f66531697a0df333db6e801e047d16db995b52
Jtobyy <jtobi8161@gmail.com>
2021-12-15 20:51:21 -0500

    Buid on 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+  double () {
+    this.height = this.height * 2;
+    this.width = this.widht * 2;
+  }
+};
