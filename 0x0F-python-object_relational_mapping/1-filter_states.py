addb9163a573c35423e2079c9db23cab157c89f7
Jtobyy <jtobi8161@gmail.com>
2021-12-21 10:44:36 +0000

    Task 2: Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Use MySQLdb to query a database for states whose names
+start with the letter N
+"""
+import sys
+import MySQLdb
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                       passwd=password, db=database, charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+
+cur.close()
+conn.close()
+
+if "__name__" == "__main__":
+    pass
