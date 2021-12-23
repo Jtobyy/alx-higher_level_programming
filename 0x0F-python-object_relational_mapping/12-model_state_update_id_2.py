aa2a26cb2596e0f669d81dfee552fcc2d4a6806f
Jtobyy <jtobi8161@gmail.com>
2021-12-23 14:23:40 +0000

    Task 12: changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+changes the name of a state object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
+@localhost/{sys.argv[3]}', pool_pre_ping=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+state = session.query(State).filter(State.id == 2).first()
+state.name = 'New Mexico'
+session.commit()
+
+
+if __name__ == '__main__':
+    pass
