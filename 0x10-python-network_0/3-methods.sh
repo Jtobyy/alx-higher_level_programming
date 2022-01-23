960a936cfb8da3a6c8ec88e18f34409956be3326
root <root@Botoby.localdomain>
2022-01-23 20:47:03 +0100

    Switch options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a DELETE request to the URL, and displays the body of the response
-curl -s -I  "$1" | grep "Allow" | cut -f 1 -d " " -d ":" --complement
+curl -s -I "$1" | grep "Allow" | cut -f 1 -d ":" -d " " --complement
