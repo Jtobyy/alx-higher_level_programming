94af08e4be834e9211b1e2e5651f4732ecad2101
root <root@Botoby.localdomain>
2022-01-24 02:17:57 +0100

    Testing solutions

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.'
-curl -s -G 0.0.0.0:5000/catch_me_3
+curl -s -L -X "PUT" 0.0.0.0:5000/catch_me
