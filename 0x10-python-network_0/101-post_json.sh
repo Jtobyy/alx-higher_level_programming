69c025d9b9b8c7c81bab6d7b7df0bbf5f0d747b7
root <root@Botoby.localdomain>
2022-01-24 02:00:00 +0100

    Run in silent mode -s

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl --data-binary @"$2" -H "Content-Type: application/json" -X "POST" "$1"
+curl -s --data-binary @"$2" -H "Content-Type: application/json" -X "POST" "$1"
