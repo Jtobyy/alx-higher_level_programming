bdae5665f9246fce6160bdb3f5855db33089e80f
root <root@Botoby.localdomain>
2022-01-28 02:14:44 +0100

    Main script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    html = response.info()
+
+print(html['X-Request-Id'])
