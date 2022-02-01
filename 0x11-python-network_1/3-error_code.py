12831e085611b69abb5d9a564ad3faaada19c6a1
root <root@Botoby.localdomain>
2022-02-01 20:11:34 +0100

    Testing

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,6 +2,7 @@
 """Takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
+
 from urllib.error import HTTPError
 import urllib.request
 import sys
