bf435c2b600abe3c424d719cb12784fb7ed6224c
root <root@Botoby.localdomain>
2022-02-01 19:06:20 +0100

    complete script

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-'''
+"""
 Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
-'''
+"""
 import urllib.parse
 import urllib.request
 import sys
 
 
 def main():
+    """
+    Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+    """
     url = sys.argv[1]
     email = sys.argv[2]
     req = urllib.request.Request(url)
