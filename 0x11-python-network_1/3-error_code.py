54051abd7fcf3c422005c6538ddc98a7e3660e12
root <root@Botoby.localdomain>
2022-02-01 20:49:47 +0100

    Testing

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
     try:
+        url = sys.argv[1]
         req = urllib.request.Request(url)
         with urllib.request.urlopen(req) as response:
             html = response.read()
