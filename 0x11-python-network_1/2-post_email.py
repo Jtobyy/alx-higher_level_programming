73301c5d8782ce64327d8fc072f505acaab01e0c
root <root@Botoby.localdomain>
2022-02-01 20:04:07 +0100

    Testing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
+"""Takes in a URL and an email, sends a POST to the requested
+URL with the email as the parameter and displays the response
 """
-Takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
-"""
-import urllib.parse
-import urllib.request
-import sys
 
+import sys
+import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
-    """
-    Takes in a URL and an email, sends a POST
-    request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-    """
     url = sys.argv[1]
-    email = sys.argv[2]
-    req = urllib.request.Request(url)
-    values = {'email': email}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # data should be bytes
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
+    value = {'email': sys.argv[2]}
+    parameter = urllib.parse.urlencode(value).encode('ascii')
 
-    print(f'{html.decode("utf-8")}')
+    request = urllib.request.Request(url, parameter)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
