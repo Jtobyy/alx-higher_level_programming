26ff4e5901e797ef57dd67f0952353e39ad1eb6d
Jtobyy <jtobi8161@gmail.com>
2022-05-26 15:42:02 +0100

    Test case for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 num_str = str(number)
 last_digit = int(num_str[-1])
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} \
-and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} \
+if last_digit == 0:
+    print(f"Last digit of {number:d} is {last_digit:d} \
 and is 0")
+elif num_str[0] == '-':
+    print(f"Last digit of {number:d} is -{last_digit:d} \
+and is less than 6 and not 0")
+elif last_digit > 5:
+    print(f"Last digit of {number:d} is {last_digit:d} \
+and is greater than 5")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} \
+    print(f"Last digit of {number:d} is {last_digit:d} \
 and is less than 6 and not 0")
