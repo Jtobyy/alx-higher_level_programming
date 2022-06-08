b5e2c80ca1bfbb6a2531ac8d6c250936bd4ad982
Jtobyy <jtobi8161@gmail.com>
2022-06-08 13:34:25 +0100

    Correct format

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,22 +8,25 @@ import sys
 import MySQLdb
 
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                       passwd=password, db=database, charset="utf8")
+if __name__ == "__main__":
+    """ Uses python MySQLdb to query a database """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
 
-cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-INNER JOIN states WHERE cities.state_id = states.id ORDER BY cities.id ASC")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    INNER JOIN states WHERE cities.state_id = states.id ORDER BY \
+    cities.id ASC")
 
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print("({}, '{}', '{}')".format(row[0], row[1], row[2]))
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
     pass
