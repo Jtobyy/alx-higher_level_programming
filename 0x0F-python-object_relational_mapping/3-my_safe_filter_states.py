f48ba3bc1199ec704eecdd24f29bd7631ad64470
Jtobyy <jtobi8161@gmail.com>
2022-06-08 13:26:29 +0100

    documentation, no execution when imported, case sensitivity

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,28 +8,28 @@ import sys
 import MySQLdb
 
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-search = sys.argv[4]
-
-for ch in search:
-    if ch == ';':
-        break;
-
-else:
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                           passwd=password, db=database, charset="utf8")
-
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name = '{search}' ORDER BY id ASC")
-
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-
-    cur.close()
-    conn.close()
-
 if __name__ == "__main__":
-    pass
+    """ Uses python MySQLdb to query a database """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    for ch in search:
+        if ch == ';':
+            break
+
+    else:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=database, charset="utf8")
+
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM states WHERE name = binary '{search}' ORDER\
+        BY id ASC")
+
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print("({}, '{}')".format(row[0], row[1]))
+
+        cur.close()
+        conn.close()
