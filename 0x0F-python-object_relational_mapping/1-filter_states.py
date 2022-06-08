538dea540f9b3d5fea3b85a5e0cb4797bee89053
Jtobyy <jtobi8161@gmail.com>
2022-06-08 12:50:14 +0100

    Lowercase n

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' OR name LIKE 'n%' \
+    ORDER BY id ASC")
 
     query_rows = cur.fetchall()
     for row in query_rows:
