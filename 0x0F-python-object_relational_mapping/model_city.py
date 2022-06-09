140e586c1737eb6ea9a15bbd0579e9bcc58e0338
Jtobyy <jtobi8161@gmail.com>
2022-06-09 01:20:04 +0100

    Task 14. Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,10 +2,10 @@
 """
 contains the class definition of a City.
 """
-
 from model_state import Base
 from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
 from sqlalchemy.orm import relationship
+
 
 class City(Base):
     __tablename__ = 'cities'
