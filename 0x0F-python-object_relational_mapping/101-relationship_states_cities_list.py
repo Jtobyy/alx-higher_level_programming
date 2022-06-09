dbda404de316261cfb3c96ac7c40e991b4c71b2a
Jtobyy <jtobi8161@gmail.com>
2022-06-09 04:00:34 +0100

    Testing

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,5 +21,4 @@ if __name__ == "__main__":
 
     for state in session.query(State).order_by(State.id):
         print('{}: {}'.format(state.id, state.name))
-        for city in state.cities:
-            print('    {}: {}'.format(city.id, city.name))
+        print('    {}:'.format(state.cities))
